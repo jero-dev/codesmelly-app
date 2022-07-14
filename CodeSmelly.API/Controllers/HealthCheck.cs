3698c582c024cdd803d629dca9a664bb340e0fa2
Jerónimo Chaves Caballero <jerocc_dev@outlook.es>
2022-07-14 20:27:54 +0200

    Init project

---
+++ CodeSmelly.API/Controllers/HealthCheck.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSmelly.API.Controllers;
+
+[ApiController]
+public class HealthCheck : ControllerBase
+{
+    [HttpGet("api/health")]
+    public IActionResult IsAlive()
+    {
+        return Ok("I'm alive!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Code Smelly App
+
+This project is a .NET API which purpose is being use for getting use to solve problems in SonarQube.
+
+#### Authory
+
+This project was made by [Jerónimo Chaves Caballero](https://github.com/jero-dev)
